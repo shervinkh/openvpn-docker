75a19b34b40ee515ca2b07e2f0114fa90ea4a060
Shervin Khastoo <shervin@shervin.org>
2017-01-12 13:25:41 +0000

    bugfix

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -22,7 +22,7 @@ if [ ${IPV4_PRIVATE_SUBNET} ]; then
 iptables -t nat -C POSTROUTING -s ${IPV4_PRIVATE_SUBNET} -o eth0 -j MASQUERADE || iptables -t nat -A POSTROUTING -s ${IPV4_PRIVATE_SUBNET} -o eth0 -j MASQUERADE
 fi
 
-if [ ${IPV6_PRIVATE_SUBNET} ] then
+if [ ${IPV6_PRIVATE_SUBNET} ]; then
 my_ipv6=$(ip -o -6 addr show eth0 | sed -e 's/^.*inet6 \([^ ]\+\).*/\1/' | head -n 1 | cut -d/ -f 1)
 ip6tables -t nat -C POSTROUTING -o eth0 -s ${IPV6_PRIVATE_SUBNET} -j SNAT --to-source $my_ipv6 || ip6tables -t nat -A POSTROUTING -o eth0 -s ${IPV6_PRIVATE_SUBNET} -j SNAT --to-source $my_ipv6
 fi
